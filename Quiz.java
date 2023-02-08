e1887415da5b03b15ae04fec53e22d3d4ff8dfdd
gillianher <gillianherault@gmail.com>
2023-02-08 11:35:38 +0100

    Quiz

--- Quiz.java
+++ Quiz.java
@@ -15,33 +15,44 @@ public class Quiz {
         ResultSet geo;
         int pts=10;
         String tent;
+        String query;
+        int theme=0;
 
-        System.out.println(" ________________________________________________________________________________");
-        System.out.println("|       Bienvenue dans ce quiz de géographie, vous partirez avec 10 points       |");
-        System.out.println("|   Vous aurez une série de 10 questions qui détermineront votre nombre de points|");
-        System.out.println("|________________________________________________________________________________|");
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println(" ________________________________________________________________________________________");
+        System.out.println("|       Bienvenue dans ce quiz de géographie, vous partirez avec 10 points               |");
+        System.out.println("|   Vous aurez ensuite une série de 10 questions qui détermineront votre nombre de points|");
+        System.out.println("|________________________________________________________________________________________|");
+        System.out.println("");
+        System.out.println("Choisis un theme en indiquant son numero : ");
+        System.out.println("1/ Géographie     2/ Histoire");
+        theme= Integer.parseInt(sc.nextLine());
+
+        //Si on veut faire différents themes    On peut aussi le faire en faisant plusieurs BDD
+
+        query=String.format("Select * from quizz Where theme=%d",theme);
         try{
             connection = DriverManager.getConnection(url,"root","");
             stmt = connection.createStatement();
-            geo = stmt.executeQuery("Select * from geographie");
+
+            geo = stmt.executeQuery(query);
 
             System.out.println(geo);
             while (geo.next()){
                 System.out.println("Question : "+geo.getString("Intitule")+"?");
-                Scanner sc=new Scanner(System.in);
+
                 System.out.println("Votre réponse : ");
                 tent=sc.nextLine();
-                if (!tent.equals(geo.getString("reponse"))){
-                    pts--;
-                }else{
+
                 while (!tent.equals(geo.getString("reponse"))){
-                    System.out.println("C'est incorrect, votre réponse : ");
+                    System.out.println("C'est incorrect, nous vous avons demandé "+geo.getString("Intitule"));
                     tent=sc.nextLine();
                     pts--;
                 }
                 pts++;
-            }}
-            System.out.println("Félicitations vous avez eu "+pts+" points!");
+            }
+            System.out.println("Félicitations vous avez eu "+pts+" points sur 20 possibles!");
 
 
     } catch (SQLException e) {
